883ff05bfd2326dd18be5fcc1c5fa9f6ae4ed823
Thibaut Lopez <thib.l.sevilla@gmail.com>
2023-01-18 12:53:28 +0100

    Enemy Manager + room static

---
+++ FrictionalGamesJam/Assets/Scripts/EnemiesManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesManager : MonoBehaviour
+{
+    public GameObject enemiesGameObject;
+    [HideInInspector] public List<EnemyController> enemiesList;
+
+    private void Start()
+    {
+        int numOfChildren = enemiesGameObject.transform.childCount;
+        for (int i = 0; i < numOfChildren; i++)
+        {
+            EnemyController enemy = enemiesGameObject.transform.GetChild(i).gameObject.GetComponent<EnemyController>();
+            enemiesList.Add(enemy);
+        }
+    }
+}
