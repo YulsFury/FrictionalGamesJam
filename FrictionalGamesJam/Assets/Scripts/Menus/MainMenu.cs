49b60d0788c62cbd2eaeb92b92e9560581df5f9c
Thibaut Lopez <thib.l.sevilla@gmail.com>
2023-01-20 18:04:44 +0100

    Password prototype

--- FrictionalGamesJam/Assets/Scripts/Menus/MainMenu.cs
+++ FrictionalGamesJam/Assets/Scripts/Menus/MainMenu.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public string LevelName;
+    public Button startButton;
+    public TMPro.TMP_InputField passwordInput;
+
+    public string password;
 
     public void StartGame()
     {
@@ -15,5 +20,13 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void CheckPassword()
+    {
+        if(password == passwordInput.text)
+        {
+            startButton.interactable = true;
+        }
     }
 }
